117359644489c6624f0ca1857260096c79a361d6
akazimadan4-source <akazimadan4@gmail.com>
2026-08-07 20:57:25 +0700

    Update WAVEX_HUB.lua

--- WAVEX_HUB.lua
+++ WAVEX_HUB.lua
@@ -17,7 +17,7 @@ StarterGui:SetCore("SendNotification", {
 task.wait(8)
 StarterGui:SetCore("SendNotification", {
     Title = "❗",
-    Text = "leaked by king vyylora",
+    Text = "tunggu sebentar ya suki",
     Duration = 8, -- seconds the notification stays on screen
     Button1 = "Got it"
 })
